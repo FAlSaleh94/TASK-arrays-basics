e8f85d87ba2d805f956e589885faa4c918ade0e1
F.AlSaleh <94alsaleh@gmail.com>
2021-12-23 10:37:41 +0300

    Tasks

---
+++ index.js
@@ -0,0 +1,21 @@
+emptyArray = [];
+hobbies = ["Gaming", "Anime", "Shopping", "learing language","TV Shows", "Reading"];
+numbers = [4, 1, 9, 10, 6];
+console.log(numbers[1]);
+//console.log(hobbies[5]);
+const Item = hobbies[hobbies.length-1]
+console.log(Item)
+hobbies.push("climing")
+console.log(hobbies[6]);
+console.log(hobbies.length);
+// you can do it in a one single line
+hobbies.pop();
+hobbies.pop();
+console.log(hobbies.length)
+// you can do it in a one single line
+hobbies.pop();
+hobbies.pop();
+hobbies.pop();
+hobbies.pop();
+hobbies.pop();
+console.log(hobbies)
